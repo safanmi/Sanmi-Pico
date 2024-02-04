bd0b7bdc23a209e747ed62a1f9726c70175ffe99
safanmi <sanmi.sanmiolu@gmail.com>
2024-02-04 10:26:34 +0100

    Updates solOptimized

--- solOptimized.py
+++ solOptimized.py
@@ -3,10 +3,8 @@ Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 List = [165,248,94,346,299,73,198,221,313,137,205,87,336,110,186,69,223,213,216,216,177,138]
 
 for items in List:
-    mod = items % 37
 
-    if ((mod == 36) | (0 <= mod <= 25) | (26 <= mod <= 35)):
-       print (Alphabets[mod])
+  print (Alphabets[items % 37])
 
    
 
